6345833bb1013460ca4fc098ec5db10a56fdc6ce
Student <fas@gmail.com>
2024-01-15 08:42:57 +0100

    test

--- src/Zadanie22.java
+++ src/Zadanie22.java
@@ -6,7 +6,7 @@ public class Zadanie22 {
         double cenaProwizji = akcje*cena*0.02;
         double cenaAkcji = akcje*cena;
         double cenaKoncowa = cenaAkcji+cenaProwizji;
-        Scaner myScanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
         double zysk = myScanner.nextDouble();
         System.out.println(cenaKoncowa + " "+ cenaAkcji + " "+ cenaProwizji + " "+ zysk);
 
