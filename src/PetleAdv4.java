5524c609bc08e961c0c73fcc17ddb7bf92c6ed10
Student <fas@gmail.com>
2024-01-15 11:29:46 +0100

    Push

--- src/PetleAdv4.java
+++ src/PetleAdv4.java
@@ -4,36 +4,45 @@ public class PetleAdv4 {
     public static void main(String[] args){
         int ppoints =0;
         int cpoints =0;
-        int computerPick = (int)(Math.random()*3)+1;
-        System.out.println(computerPick);
-        Scanner myScan = new Scanner(System.in);
-        int player = myScan.nextInt();
-        if(player==3 && computerPick==1){
-            ppoints++;
+        while(ppoints<=3 || cpoints<=3){
+            int computerPick = (int)(Math.random()*3)+1;
+            System.out.println(computerPick);
+            Scanner myScan = new Scanner(System.in);
+            int player = myScan.nextInt();
+            if(player==3 && computerPick==1){
+                ppoints++;
+            }
+            else if(player==2 && computerPick==3){
+                ppoints++;
+            }
+            else if(player==1 && computerPick==2){
+                ppoints++;
+            }
+            else if(player==1 && computerPick==3){
+                cpoints++;
+            }
+            else if(player==2 && computerPick==1){
+                cpoints++;
+            }
+            else if(player==3 && computerPick==2){
+                cpoints++;
+            }
+            if(player==3 && computerPick==1){
+                ppoints++;
+            }
+            else if(player==2 && computerPick==3){
+                ppoints++;
+            }
+            else if(player==1 && computerPick==2){
+                ppoints++;
+            }
+            System.out.println(ppoints + " " + cpoints);
         }
-        else if(player==2 && computerPick==3){
-            ppoints++;
+        if(cpoints>ppoints){
+            System.out.println("Konkuter wygrał");
         }
-        else if(player==1 && computerPick==2){
-            ppoints++;
-        }
-        else if(player==1 && computerPick==3){
-            cpoints++;
-        }
-        else if(player==2 && computerPick==1){
-            cpoints++;
-        }
-        else if(player==3 && computerPick==2){
-            cpoints++;
-        }
-        if(player==3 && computerPick==1){
-            ppoints++;
-        }
-        else if(player==2 && computerPick==3){
-            ppoints++;
-        }
-        else if(player==1 && computerPick==2){
-            ppoints++;
+        else{
+            System.out.println("Gracz wygrał");
         }
     }
 }
